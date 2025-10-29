076e8bb5412d030a7c1d4ef4b07383877319fa8c
man-in-dev <man.in.dev.16@gmail.com>
2025-10-29 16:50:06 +0530

    added base url

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: '/apiic_admin',
+    output: 'standalone', // optional but helps in production
     env: {
         API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:3000/api',
     },
